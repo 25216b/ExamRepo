e33871a35ef1787332e1c8aa86e210802cf522db
25216b <25216@ecam.be>
2025-12-18 01:13:24 +0100

    fix navbar

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -104,7 +104,7 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="fixed top-0 left-0 right-0 w-full py-4 px-4 z-[9999]">
+        <nav className="sticky top-0 w-full py-4 px-4 z-50 bg-gray-900">
             <div className="max-w-2xl mx-auto bg-black rounded-full px-6 py-3 relative shadow-lg">
                 <ul className="list-none flex justify-around items-center">
                     <NavLink href="#top">🏠</NavLink>
